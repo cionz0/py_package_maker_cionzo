0b753661c9c316949f844f8fb69d3f21a36ba7b8
cionzo <vincenzo@filotrack.com>
2023-01-13 10:03:24 +0100

    introduced main

---
+++ src/py_package_maker_cionzo/pyprojecttoml/__main__.py
@@ -0,0 +1,19 @@
+""" This module is a module
+
+    :synopsis: A useful module indeed.
+    :position: src.py_package_maker_cionzo.pyprojecttoml.__main__.py
+    :author: cionzo <cionzo@filotrack.com>
+    :created on: 13/01/23
+    :project: py_package_maker_cionzo
+ """
+
+
+from typing import List, Optional
+
+__version__ = "0.0.1"
+
+
+def main(args: Optional[List[str]] = None) -> int:
+
+    print(args)
+    return 0
