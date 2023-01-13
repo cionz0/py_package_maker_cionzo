3792fefd842395428070c67f85022ed248dd46b6
cionzo <vincenzo@filotrack.com>
2023-01-13 10:17:41 +0100

    fix

---
+++ src/py_package_maker_cionzo/license/__init__.py
@@ -0,0 +1,8 @@
+""" This module is a module
+
+    :synopsis: A useful module indeed.
+    :position: src.py_package_maker_cionzo.license.__init__.py
+    :author: cionzo <cionzo@filotrack.com>
+    :created on: 13/01/23
+    :project: py_package_maker_cionzo
+ """
